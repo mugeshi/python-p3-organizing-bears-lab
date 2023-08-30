bdcac8d44fa56939b52fb9cdf1d3af2a77464712
mugeshi <nicolemugeshi@gmail.com>
2023-08-30 14:22:36 +0300

    done

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+  //columns here
+
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperature TEXT,
+  alive BOOLEAN
+  );
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+        VALUES 
+            ("Mr. Chocolate", 3, "M", "black", "extraversion", True),
+            ("Rowdy", 5, "F", "grey", "extraversion", False),
+            ("Tabitha", 9, "M", "black", "neuroticism", True),
+            ("Sergeant Brown", 11, "F", "black", "extraversion", True),
+            ("Melissa", 15, "M", "black", "agreeableness", True),
+            ("Grinch", 12, "F", "black", "extraversion", True),
+            ("Wendy", 3, "M", "black", "extraversion", True),
+            (NULL, 7, "F", "black", "agreeableness", True);
